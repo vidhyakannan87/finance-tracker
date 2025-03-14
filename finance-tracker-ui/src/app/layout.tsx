384ee4f17dc93caf3f514fe8fba18636f7e98af4
Vidhya Kannan <vidhya.kannan@hingehealth.com>
2025-03-14 17:46:59 -0400

    Revert "Revert "Revert "Revert "Inital UI commit""""
    This reverts commit 6a1daeeb838c07682fc11f6cf3ae9022bc2da40e.

---
+++ finance-tracker-ui/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { AuthProvider } from "./context/AuthContext";
+import Header from "./components/Header";
+
+export const meta: Metadata = {
+  title: "Finance Tracker",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Header />
+          <main>{children}</main>
+        </body>
+      </html>
+    </AuthProvider>
+  );
+}
